d565002ef60f035c18cad821147e6ad6d1b5a73c
hll <376869908@qq.com>
2018-05-16 18:55:41 +0800

    Merge branch 'master' of 192.168.1.8:root/policy_answer
    # Conflicts:
    #	src/store/index.js

--- src/pages/policydetails/policydetails.vue
+++ src/pages/policydetails/policydetails.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <div class="title">政策百科</div>
+    <div class="release-time">发布时间 : 2017-02-30</div>
+    <div class="details"></div>
   </div>
 </template>
 
@@ -9,6 +11,7 @@
     data(){
       return {}
     },
+
     methods: {},
     components: {}
 
@@ -17,5 +20,21 @@
 
 <style lang="less" scoped>
   @import '../../static/less/common.less';
-
+  .title{
+    margin: 0.512rem 0 0.234667rem;
+    text-align: center;
+    font-size: 0.341333rem;
+    color: #333333;
+  }
+  .release-time{
+    font-size: 0.256rem;
+    color: #999999;
+    text-align: center;
+  }
+  .details{
+    margin: 0.341333rem 0.533333rem 0 0.533333rem;
+    font-size: 0.277333rem;
+    line-height: 0.426667rem;
+    color: #666666;
+  }
 </style>
