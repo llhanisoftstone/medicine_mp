cd8fc80212565a13d1bf3559c0023d8cb08c6824
chenxiangdong <1425345959@qq.com>
2018-05-16 12:17:41 +0800

    个人信息组件

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
   box-sizing: border-box;
 }
 /* this rule will be remove */
---
+++ src/components/userinfo.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="user_box">
+      <image src="/static/img/touxiang.png" class="user_bg"></image>
+      <div class="user_info">
+        <div class="img_box">
+          <image :src='imgurl'></image>
+        </div>
+        <p>{{username}}</p>
+        <slot name="userRight" class="slot_box"></slot>
+      </div>
+    </div>
+</template>
+
+<script type="javascript">
+    export default {
+        name: 'userinfo',
+        props: ['imgurl','username'],
+        data(){
+            return {}
+        },
+        methods: {},
+    }
+</script>
+
+<style lang="less" scoped>
+    @import '../static/less/common.less';
+    .user_box{
+      width: 100%;
+      height: 194px/2;
+      padding: 13px/2 26px/2 15px/2;
+      position:relative;
+      .user_bg{
+        width: 698px/2;
+        height: 194px/2;
+        position: absolute;
+        top:13px/2;
+        left:26px/2;
+        z-index:0;
+      }
+      .user_info{
+        position: absolute;
+        left:26px/2;
+        top:13px/2;
+        width: 698px/2;
+        height: 194px/2;
+        z-index:2;
+        align-items: center;
+        justify-content: space-between;
+        box-sizing: border-box;
+        padding-right: 31px/2;
+        display: flex;
+        .img_box{
+          width: 135px/2;
+          height: 135px/2;
+          border-radius: 50%;
+          background: #fff;
+          padding: 4px/2;
+          overflow: hidden;
+          margin-left:50px/2;
+          img{
+            width: 127px/2;
+            height: 127px/2;
+            border-radius: 50%;
+          }
+        }
+        p{
+          margin-left: 38px/2;
+          font-size: 32px/2;
+          color: #333;
+          width: 300px/2;
+        }
+        .slot_box{
+          flex: 1;
+        }
+      }
+    }
+</style>
