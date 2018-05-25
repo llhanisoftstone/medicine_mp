a69c40838d40f7a6dd8924b1d71b7200201fd6de
chenxiangdong <1425345959@qq.com>
2018-05-25 21:42:47 +0800

    错误提示

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     url: 'https://policy.lifeonway.com',
-    // ws_server: 'https://policy.lifeonway.com',
-    ws_server: 'http://192.168.1.12:5000',
+    ws_server: 'https://policy.lifeonway.com',
+    // ws_server: 'http://192.168.1.12:5000',
     userinfo: {},    //  微信返回用户信息
     code: '',
     isauth: false,      // 是否授权
