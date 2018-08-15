3820b2e0d5b4335c940a0aedab6b1c08c70366b2
chenxiangdong <1425345959@qq.com>
2018-08-15 19:05:35 +0800

    youhua

--- src/pages/team/team.vue
+++ src/pages/team/team.vue
@@ -96,10 +96,10 @@
       </div>
       <div class="publish_box">
         <div class="btn_box_send">
-          <p @click="send('陈独秀同学请坐下')">选1</p>
-          <p @click="send('请给李时珍同学一个发言的机会')">选2</p>
-          <p @click="send('你看后面的鲁迅同学脸色很难看')">选3</p>
-          <p @click="send('还有，李云龙同学麻烦你让二营长把意大利面收起来，不要让陈皮同学在课堂上吃面。')">选4</p>
+          <p @click="send('陈独秀同学请坐下',0)">选1</p>
+          <p @click="send('请给李时珍同学一个发言的机会',1)">选2</p>
+          <p @click="send('你看后面的鲁迅同学脸色很难看',2)">选3</p>
+          <p @click="send('还有，李云龙同学麻烦你让二营长把意大利面收起来，不要让陈皮同学在课堂上吃面。',3)">选4</p>
         </div>
         <i class="quick" @click="selectQuick">常用语</i>
         <a @click="userTools(user.tools[0].amount,1)" :class="{surplus:surplus}" href="" v-if="challenger==user.userid"><image src="/static/img/daojushangdian_11.png"></image><span>{{user.tools[0].amount>99?'99+':user.tools[0].amount}}</span></a>
@@ -350,7 +350,7 @@
               this.isjoin=false
               this.is_f_click=-1
             },
-          send(msg){       //发送聊天
+          send(msg,index){       //发送聊天
             this.isquick = false
             let that =this
             if(new Date().getTime() - that.sendTime<1000){
@@ -366,6 +366,9 @@
                   data:msg,
                   type:1
               })
+            if(that.challenger != that.$store.state.user.userid){
+              that.submit(index)
+            }
           },
           submit(index,right){    //提交答案
             if(this.isclick){
