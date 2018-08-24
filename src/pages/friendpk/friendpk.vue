09770444142cf1228274813978be34a1fb89d601
546485659@qq.com <546485659@qq.com>
2018-08-24 18:24:14 +0800

    bug

--- src/pages/friendpk/friendpk.vue
+++ src/pages/friendpk/friendpk.vue
@@ -76,7 +76,7 @@
             }
           })
         }else if(this.isFriends==3){
-          this.$get('/rs/company_experience',{order:'experience desc,create_time',page:1,size:50}).then(res=>{
+          this.$get('/rs/company_experience',{order:'experience desc,id',page:1,size:50}).then(res=>{
             if(res.code == 200){
               for(var i=0;i<res.rows.length;i++){
                 if(res.rows[i].avatar_url==""||res.rows[i].avatar_url==null){
