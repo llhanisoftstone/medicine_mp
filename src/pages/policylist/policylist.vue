e7d181c315e5505ee662260bbc9c3b6356b3c32c
GaoXinwu <gaoxinwucn@foxmail.com>
2018-09-14 19:33:41 +0800

    22164 （前台）政策百科，列表图加载错误，前台直接不显示图片；预期：显示一个默认图片

--- src/pages/policylist/policylist.vue
+++ src/pages/policylist/policylist.vue
@@ -103,10 +103,11 @@
           if (res.rows.length > 0){
             for (let i=0; i<res.rows.length; i++){
               res.rows[i].view_count = that.commons.zcount(res.rows[i].view_count)
-//              if(res.rows[i].pic_abbr){
-//                res.rows[0].pic_abbr= '/static/img/giftshop_moren2.jpg';
-//              }
-              res.rows[i].pic_abbr = that.$store.state.url+res.rows[i].pic_abbr;
+              if(!res.rows[i].pic_abbr || res.rows[i].pic_abbr==''){
+                res.rows[i].pic_abbr= '/static/img/policy_default.jpg';
+              }else{
+                res.rows[i].pic_abbr = that.$store.state.url+res.rows[i].pic_abbr;
+              }
             }
             that.policy_list = that.policy_list.concat(res.rows);
           }
