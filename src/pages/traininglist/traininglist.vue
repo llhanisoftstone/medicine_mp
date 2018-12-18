85c81f89dc4f5e2dcd0c8d341853541f1a586358
GaoXinwu <gaoxinwucn@foxmail.com>
2018-12-18 21:25:34 +0800

    培训数据

--- src/pages/traininglist/traininglist.vue
+++ src/pages/traininglist/traininglist.vue
@@ -138,6 +138,7 @@
             this.photo = false;
           }else if(this.curTab == 2){
             this.photo = true;
+
           }
           this.traininglist();
         },
@@ -155,13 +156,15 @@
             page:this.page,
             size:this.size,
             order:"count desc",
-            status:"=,2"
           };
-          if(this.curTab == 1){
+          if(that.curTab == 1){
+            data.status=2;
             data.end_time=">=,"+this.dataTime
           }
-          if(this.curTab == 2){
-            data.end_time="<,"+this.dataTime
+          if(that.curTab == 2){
+            data.ins=['status','3','10','11','12','13']
+            //data.end_time="<,"+this.dataTime
+
           }
           let res = await that.$get('/rs/activity_app',data);
           if (res.code == 200){
